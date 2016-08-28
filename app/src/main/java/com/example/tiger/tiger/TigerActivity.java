a7817bab660790fc3e24c0faa3751583002c4f5e
anup <anupnepal@gmail.com>
2016-08-27 19:48:12 -0700

    start

---
+++ app/src/main/java/com/example/tiger/tiger/TigerActivity.java
@@ -0,0 +1,13 @@
+package com.example.tiger.tiger;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class TigerActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tiger);
+    }
+}
